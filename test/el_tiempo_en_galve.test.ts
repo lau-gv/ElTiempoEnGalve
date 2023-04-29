677b93376f37d2dfe41269976b845738abfa5fdb
Laura Gimeno Villanueva <lauragimenocp@gmail.com>
2023-04-29 17:50:03 +0200

    Initial commit

---
+++ test/el_tiempo_en_galve.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ElTiempoEnGalve from '../lib/el_tiempo_en_galve-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/el_tiempo_en_galve-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ElTiempoEnGalve.ElTiempoEnGalveStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
