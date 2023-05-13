b17c30c80ba372a1e36ad5ff11f3bf2059c6a423
Laura Gimeno Villanueva <lauragimenocp@gmail.com>
2023-05-13 11:46:50 +0200

    mmm

--- src/receiverService/controller/insertDataController.ts
+++ src/receiverService/controller/insertDataController.ts
@@ -13,8 +13,9 @@ export async function insertData(event: any, TABLE_NAME: string, dbClient : Dyna
     }catch(Error: any){
 
         /*¿Por qué no controlo este error aquí? Porque quiero que lo controle step functions
-        y no mi código, así, si algo de la inserción de datos falla, se marca como que ha fallado
-        y por tanto el mensaje se devuelve a la cola. (Duda, ver cómo se gestiona eso...)*/
+        y no mi código, así, si algo de la inserción de datos falla, se marca como que ha fallado.
+        Si quien leyese fuese una función lambda, el mensaje se devolvería a la cola y habría manejo de errores 
+        de forma integrada, pero, al estar lanzando step functions, no sé que pasa. EN PENDIENTES MANEJO DE ERRORES*/
         console.log(`Errorcito!: ${Error.message}`);
         throw new Error(`${Error.message}`);
     }
